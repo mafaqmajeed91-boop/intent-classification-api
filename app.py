e2e11266dbb3dc0c245dfc7263fb347fc897ef90
mafaqmajeed91-boop <mafaqmajeed91@gmail.com>
2026-06-04 13:31:56 +0500

    app.py

--- app.py
+++ app.py
@@ -44,3 +44,10 @@ async def predict(items: List[Item]):
         })
 
     return results
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API is working 🚀"
